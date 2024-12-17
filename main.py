4a042dc0c0fd3daa6c270df3189754475ea090d5
Daniel-Poli-code <dpolishchuk216@gmail.com>
2024-12-17 10:13:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_openml
 
+data = fetch_openml(name='diabetes', version=1, as_frame=True)
+
+df = data.frame
+
+features = list(df.columns)
+
+fig, axs = plt
